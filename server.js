c2b4c1037b8375196d11c86e4bef9f459cfca2c7
Sagar Dobariya <infodsagar@gmail.com>
2022-10-19 19:43:53 +1100

    1020

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ const io = require('socket.io')(4000);
 
 //Static assests
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('front/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'fron', 'build', 'index.html'));
   });
 }
 
